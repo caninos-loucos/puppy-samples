1758034015a6a26529a09178fe98fc518f3a86b1
edgarlsitec <edgar.righi@lsitec.org.br>
2026-06-02 11:55:20 -0300

    Add log settings to hello app.

--- hello/main.c
+++ hello/main.c
@@ -1,13 +1,18 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(app, LOG_LEVEL_INF);
 
 int main(void)
 {
+    LOG_INF("Application running!"); 
+
     while (1)
     {
         printk("Hello World from Puppy!\n");
         k_msleep(1000);
     }
+
     return 0;
 }
-
